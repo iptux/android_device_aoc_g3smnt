9a81155c681df3eeb80a74a1965e0cc164a1022c
Tommy Alex <iptux7@gmail.com>
2016-01-11 20:00:25 +0800

    g3smnt: remove duplicate files
    Signed-off-by: Tommy Alex <iptux7@gmail.com>

--- device_g3smnt.mk
+++ device_g3smnt.mk
@@ -56,7 +56,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml \
 
 # Build characteristics setting
 PRODUCT_CHARACTERISTICS := tablet
